23f82eb2b40f2ebd98d6db8f1772bd73d8192be5
Simone Sammarco <simone.sammarco@itsincom.it>
2026-01-24 14:11:49 +0100

    Creazione del contesto e della factory del contesto

--- B_Infrastructure/db/SnakeDbContext.cs
+++ B_Infrastructure/db/SnakeDbContext.cs
@@ -11,9 +11,6 @@ namespace B_Infrastructure.db
 {
     public class SnakeDbContext : DbContext 
     {
-        // costruttore solito 
-        // collections 
-        // on model creating con la mia configurazione figa 
         public SnakeDbContext(DbContextOptions options) : base(options){}
 
         public DbSet<User> Users { get; set; }
@@ -21,7 +18,7 @@ namespace B_Infrastructure.db
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.ApplyConfigurationsFromAssembly(typeof(GameConfig).Assembly)
+            modelBuilder.ApplyConfigurationsFromAssembly(this.GetType().Assembly);
         }
     }
 }
--- B_Infrastructure/db/dbContextFactory.cs
+++ B_Infrastructure/db/dbContextFactory.cs
@@ -1,18 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore.Design;
-using Microsoft.EntityFrameworkCore.Metadata.Builders;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Configuration.Json;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.Extensions;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Options;
-using Microsoft.EntityFrameworkCore.SqlServer;
 
 namespace B_Infrastructure.db
 {
@@ -21,17 +9,20 @@ namespace B_Infrastructure.db
 
         public SnakeDbContext CreateDbContext(string[] args)
         {
-            IConfigurationRoot c = new ConfigurationBuilder()
+            IConfigurationRoot confBuilder = new ConfigurationBuilder()
                 .SetBasePath(Directory.GetCurrentDirectory())
-                .AddJsonFile("root")
+                .AddJsonFile("appsettings.json")
                 .Build();
 
             DbContextOptionsBuilder<SnakeDbContext> optionsBuilder = new();
-            string conn = c["Database:ConnectionString"] ?? "";
+            string conn = confBuilder["ConnectionStrings:Default"];
+            if(conn == null)
+            {
+                throw new InvalidOperationException("Connection string 'Default' not found.");
+            }
             optionsBuilder.UseSqlServer(conn);
 
             return new SnakeDbContext(optionsBuilder.Options);
         }
     }
-
 }
