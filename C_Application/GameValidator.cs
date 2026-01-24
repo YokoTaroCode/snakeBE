74a0c7c5da6f4b56ef7fa871b746dbdc6837e743
Simone Sammarco <simone.sammarco@itsincom.it>
2026-01-24 14:15:48 +0100

    Creazione dei repo

---
+++ C_Application/GameValidator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using B_Infrastructure.dto;
+using FluentValidation;
+
+namespace C_Application
+{
+    public class GameValidator : AbstractValidator<GameDto>
+    {
+        public GameValidator() 
+        { 
+            RuleFor(game => game.Points)
+                .NotEmpty().WithMessage("Il punteggio è richiesto")
+                .GreaterThanOrEqualTo(0).WithMessage("Il punteggio deve essere maggiore di 0");
+
+            RuleFor(game => game.Time)
+                .NotEmpty().WithMessage("Il tempo di gioco è richiesto")
+                .GreaterThanOrEqualTo(0).WithMessage("la data deve essere maggiore di 0");
+        }
+
+    }
+}
---
+++ C_Application/UserValidator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using B_Infrastructure.dto;
+using B_Infrastructure.dtodotnet;
+using FluentValidation;
+
+namespace C_Application
+{
+    public class UserValidator : AbstractValidator<UserDto>
+    {
+        public UserValidator()
+        {
+            RuleFor(user => user.Username)
+                .NotEmpty().WithMessage("è richiesto uno username")
+                .MinimumLength(8).WithMessage(" Lo username deve essere di almeno 8 caratteri");
+
+            RuleFor(user => user.Password)
+                .NotEmpty().WithMessage("è richiesta una password")
+                 .MinimumLength(8).WithMessage(" La password deve essere di almeno 8 caratteri");
+        }
+    }
+}
